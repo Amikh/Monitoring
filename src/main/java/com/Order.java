71def4611a469e138855dbd697570011b42f7935
AM <amikh05@gmail.com>
2020-04-13 10:27:38 +0300

    first commit

---
+++ src/main/java/com/Order.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Order {
+    int amount;
+    String type;
+
+    public Order(int amount, String type) {
+        this.amount = amount;
+        this.type = type;
+    }
+}
